87f33a8b3f105c4b1389c1899d7688ce2b5ffa93
hennsoe <hennsoe@gmail.com>
2020-12-16 11:27:08 +0100

    Update LuftesensorArduinoKode.ino

--- LuftesensorArduinoKode/LuftesensorArduinoKode.ino
+++ LuftesensorArduinoKode/LuftesensorArduinoKode.ino
@@ -1,27 +1,34 @@
+// Call the labriray
 #include "Adafruit_CCS811.h"
 Adafruit_CCS811 ccs;
+
+// Setup loop starts serial com on port: 9600
 void setup() {
   Serial.begin(9600);
   Serial.println("CCS811 test");
+  // Starts css listening via i2c
   if(!ccs.begin()){
-  Serial.println("Failed to start sensor! Please check your wiring.");
-  while(1);
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+  //calibrate temperature sensor
+  while(!ccs.available());
+    float temp = ccs.calculateTemperature();
+    ccs.setTempOffset(temp - 25.0);
 }
-//calibrate temperature sensor
-while(!ccs.available());
-  float temp = ccs.calculateTemperature();
-  ccs.setTempOffset(temp - 25.0);
-}
+
 void loop() {
   if(ccs.available()){
     float temp = ccs.calculateTemperature();
   if(!ccs.readData()){
+    // This is left as seperate, may be combined for performance
     Serial.print("CO2: ");
     Serial.print(ccs.geteCO2());
-    Serial.print("ppm, TVOC: ");
+    Serial.print("ppm.\n TVOC: ");
     Serial.print(ccs.getTVOC());
-    Serial.print("ppb Temp:");
+    Serial.print("ppb,\n Temp:");
     Serial.println(temp);
+    Serial.print("degree\n");
 }
 else{
   Serial.println("ERROR!");
